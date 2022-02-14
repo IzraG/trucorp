ae22b8652af7528489e45033a3432e42ec123406
izrag <izragiffari@gmail.com>
2022-02-14 08:03:04 -0800

    added jumlah.php

---
+++ src/jumlah.php
@@ -0,0 +1,37 @@
+<?php
+
+$conn=new mysqli("db","root","secret","Trucorp");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+
+$rows = array();
+$join = $conn->query("SELECT users.ID, users.Nama, users.Alamat, users.Jabatan FROM users");
+while($row = mysqli_fetch_assoc($join)){
+    $rows[] = $row;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+    </head>
+
+
+    <body>
+    
+    <?php $count = 0; 
+    foreach($rows as $row):?>
+        
+        <?php $count = $count+ 1?>
+
+    <?php endforeach;?>
+    <?=$count ?>
+
+    </body>
+
+
+</html> 
